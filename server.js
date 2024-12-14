e2ad119600a247b6b78faffe59e92de65c1d27d1
Nikita Usachev <guyfoks737@gmail.com>
2024-12-14 23:20:53 +0300

    minor changes

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const app = express();
 
-app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
 
-app.get("/", (req, res) => {
-  console.log("test");
-  res.render("index", { text: "World" || "Default" });
-});
+app.set("view engine", "ejs");
 
 const userRouter = require("./routes/users");
 
